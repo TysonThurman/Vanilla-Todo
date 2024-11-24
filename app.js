97bcebeddcbd83cc916aa5444eedd2896468918a
Tyson Thurman <tthurman@Tysons-Laptop.local>
2024-11-23 20:05:24 -0800

    restructure

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
-import { todo } from './todo.js';
+// import { todo } from './todo.js';
 
-var deleteBtnArray = document.getElementsByClassName("delete-todo-btn");
+//Start with empty array to hold todo tasks
+var todos = [];
 
-for (let i = 0; i < deleteBtnArray.length; i++) {
-    deleteBtnArray[i].addEventListener("click", handleDelete);
-  }
+var todoForm = document.getElementById('todo-form');
+todoForm.addEventListener("submit", handleSubmit);
 
-var editBtnArray = document.getElementsByClassName("edit-todo-btn");
+function handleSubmit(e) {
+    e.preventDefault();
+    
+    //add the todo objects to the array
+    var newTodoValue = document.getElementById('input-todo').value;
+    var id = "id" + Math.random().toString(16).slice(2);
+    var todoObject = {"id": id, "value": newTodoValue};
+    todos.push(todoObject);
+    console.log(todos);
 
-for (let i = 0; i < editBtnArray.length; i++) {
-    editBtnArray[i].addEventListener("click", handleEdit);
-  }
+    //add the todos to the DOM list
+    var ul = document.getElementById("todo-list");
+    var li = document.createElement("li");
+    li.className = 'todo-list__item';
+    li.innerHTML = `${todoObject.value} <i class="fa-solid fa-pencil todo-btn edit-todo-btn" style="color: #0a4d80;"></i><i class="fa-solid fa-trash-can todo-btn delete-todo-btn" style="color: #da1010;"></i>`
+    li.setAttribute("id", id);
+    ul.appendChild(li);
+}
 
-function handleDelete() {
-    console.log("Delete button clicked");
-}  
+//Put all buttons for the todos into an array for button click functions
+var todoButtonArray = document.getElementsByClassName("todo-btn");
+for (let i = 0; i < todoButtonArray.length; i++) {
+    todoButtonArray[i].addEventListener("click", handleClick);
+}
 
-function handleEdit() {
-    console.log("Edit button clicked");
-}  
+//Run this function when a button is clicked to edit or delete a button
+function handleClick() {
+    //if the button is a delete button, run todo.handleDelete
+    if(this.classList.contains("delete-todo-btn")) {
 
-var newToDo = new todo();
+    }
+
+    if(this.classList.contains("edit-todo-btn")) {
+
+    }
+}
