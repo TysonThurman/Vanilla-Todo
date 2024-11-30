8282f802b5d613b90e3c478c70c58af79a2cc086
Tyson Thurman <tthurman@Tysons-Laptop.local>
2024-11-29 16:26:22 -0800

    reorg

--- app.js
+++ app.js
@@ -1,44 +1,54 @@
-// import { todo } from './todo.js';
+const todoList = document.getElementById("todo-list");
 
-//Start with empty array to hold todo tasks
-var todos = [];
+var existingTodos = JSON.parse(localStorage.getItem("allTodos")) || [];
+
+if(existingTodos.length != 0) {
+    for (let i = 0; i < existingTodos.length; i++) {
+        var id = existingTodos[i]["id"];
+        var listItem = document.createElement("li");
+        listItem.className = 'todo-list__item';
+        listItem.innerHTML = `${existingTodos[i]["value"]}<i class="fa-solid fa-pencil todo-btn edit-todo-btn" style="color: #0a4d80;"></i><i class="fa-solid fa-trash-can todo-btn delete-todo-btn" style="color: #da1010;"></i>`
+        listItem.setAttribute("id", id);
+        todoList.appendChild(listItem);
+    }
+}
+
+const todoButtonArray = document.getElementsByClassName("todo-btn");
+
+for (let i = 0; i < todoButtonArray.length; i++) {
+    todoButtonArray[i].addEventListener("click", handleClick);
+}
 
 var todoForm = document.getElementById('todo-form');
 todoForm.addEventListener("submit", handleSubmit);
 
 function handleSubmit(e) {
     e.preventDefault();
-    
-    //add the todo objects to the array
+    //add the todo objects to the local storage
     var newTodoValue = document.getElementById('input-todo').value;
     var id = "id" + Math.random().toString(16).slice(2);
     var todoObject = {"id": id, "value": newTodoValue};
-    todos.push(todoObject);
-    console.log(todos);
+    localStorage.setItem("newTodo", JSON.stringify(todoObject));
+    existingTodos.push(todoObject);
+    localStorage.setItem("allTodos", JSON.stringify(existingTodos));
 
-    //add the todos to the DOM list
-    var ul = document.getElementById("todo-list");
-    var li = document.createElement("li");
-    li.className = 'todo-list__item';
-    li.innerHTML = `${todoObject.value} <i class="fa-solid fa-pencil todo-btn edit-todo-btn" style="color: #0a4d80;"></i><i class="fa-solid fa-trash-can todo-btn delete-todo-btn" style="color: #da1010;"></i>`
-    li.setAttribute("id", id);
-    ul.appendChild(li);
-}
-
-//Put all buttons for the todos into an array for button click functions
-var todoButtonArray = document.getElementsByClassName("todo-btn");
-for (let i = 0; i < todoButtonArray.length; i++) {
-    todoButtonArray[i].addEventListener("click", handleClick);
+    //add new todo to the DOM list
+    // var ul = document.getElementById("todo-list");
+    var todoItem = document.createElement("li");
+    todoItem.className = 'todo-list__item';
+    todoItem.innerHTML = `${todoObject.value} <i class="fa-solid fa-pencil todo-btn edit-todo-btn" style="color: #0a4d80;"></i><i class="fa-solid fa-trash-can todo-btn delete-todo-btn" style="color: #da1010;"></i>`
+    todoItem.setAttribute("id", id);
+    todoList.appendChild(todoItem);
 }
 
 //Run this function when a button is clicked to edit or delete a button
 function handleClick() {
     //if the button is a delete button, run todo.handleDelete
     if(this.classList.contains("delete-todo-btn")) {
-
+        console.log("delete button clicked");
     }
 
     if(this.classList.contains("edit-todo-btn")) {
-
+        console.log("Edit button clicked");
     }
 }
